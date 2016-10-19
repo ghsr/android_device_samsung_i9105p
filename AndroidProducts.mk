0aec61c5fb7fa3e481ab24d1ccdddc2865c74028
ghsr <ghsr92@yandex.ru>
2016-10-19 22:25:16 +0600

    i9105p: Remove full_i9105p.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_i9105p.mk \
-    $(LOCAL_DIR)/full_i9105p.mk
+    $(LOCAL_DIR)/omni_i9105p.mk
--- omni_i9105p.mk
+++ omni_i9105p.mk
@@ -1,19 +1,23 @@
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
 # Inherit Omni GSM telephony parts
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit Omni product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Enhanced NFC
-#$(call inherit-product, vendor/omni/config/nfc_enhanced.mk)
-
 # Inherit device configuration
-$(call inherit-product, device/samsung/i9105p/full_i9105p.mk)
+$(call inherit-product, device/samsung/i9105p/i9105p.mk)
 
-# Device identifier. This must come after all inclusions
+## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_i9105p
+PRODUCT_DEVICE := i9105p
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MODEL := GT-I9105P
 
-# Set build fingerprint / ID / Prduct Name ect.
+# Set build fingerprint / ID / Product Name etc.
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=s2vepxx \
     TARGET_DEVICE=s2vep \
